416829e5d49a29faf753fa80632cc69ab3079cc7
andrewptjio <andrewptjioo@gmail.com>
2022-01-19 12:42:34 +0800

    add repository category

---
+++ repository/category/category.go
@@ -0,0 +1,68 @@
+package category
+
+import (
+	"ecommerce/entities"
+
+	"github.com/labstack/gommon/log"
+	"gorm.io/gorm"
+)
+
+type CategoryRepository struct {
+	db *gorm.DB
+}
+
+func NewCategoryRepo(db *gorm.DB) *CategoryRepository {
+	return &CategoryRepository{db: db}
+}
+
+func (cr *CategoryRepository) GetAll() ([]entities.Category, error) {
+	categories := []entities.Category{}
+	if err := cr.db.Find(&categories).Error; err != nil {
+		log.Warn("Found database error", err)
+		return nil, err
+	}
+
+	return categories, nil
+}
+
+func (cr *CategoryRepository) Get(categoryId int) (entities.Category, error) {
+	category := entities.Category{}
+	if err := cr.db.Find(&category, categoryId).Error; err != nil {
+		log.Warn("Found database error", err)
+		return category, err
+	}
+	return category, nil
+}
+
+func (cr *CategoryRepository) Create(newCategory entities.Category) (entities.Category, error) {
+	if err := cr.db.Save(&newCategory).Error; err != nil {
+		return newCategory, err
+	}
+	return newCategory, nil
+}
+
+func (cr *CategoryRepository) Update(newCategory entities.Category, categoryId int) (entities.Category, error) {
+	category := entities.Category{}
+	if err := cr.db.Find(&category, "id=?", categoryId).Error; err != nil {
+		return newCategory, err
+	}
+
+	category = newCategory
+
+	if err := cr.db.Save(&category).Error; err != nil {
+		return newCategory, err
+	}
+
+	return newCategory, nil
+}
+
+func (cr *CategoryRepository) Delete(categoryId int) (entities.Category, error) {
+	category := entities.Category{}
+	if err := cr.db.Find(&category, "id=?", categoryId).Error; err != nil {
+		return category, err
+	}
+	if err := cr.db.Delete(&category).Error; err != nil {
+		return category, err
+	}
+	return category, nil
+}
