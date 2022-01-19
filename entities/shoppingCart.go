1b0b95bbc057c1b32912aa3350dda582eed0d118
andrewptjio <andrewptjioo@gmail.com>
2022-01-19 17:28:29 +0800

    entities change

--- entities/shoppingCart.go
+++ entities/shoppingCart.go
@@ -3,10 +3,10 @@ package entities
 type ShoppingCart struct {
 	// gorm.Model
 	//SHOPPINGCARTID AUTO GENERATE
-	ID        uint     `gorm:"primary_key:auto_increment" json:"id" form:"id"`
-	User      User     `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
-	UserID    uint     `gorm:"unique;not null" json:"user_id" form:"id"`
-	ProductID uint     `gorm:"unique;not null" json:"product_id" form:"product_id"`
-	Order     []*Order `gorm:"many2many:shoppingcart_order;"`
-	Qty       int      `json:"qty" form:"qty"`
+	ID        uint `gorm:"primary_key:auto_increment" json:"shoppingcart_id" form:"shoppingcart_id"`
+	OrderId   uint `json:"order_id" form:"order_id"`
+	UserID    uint `gorm:"not null" json:"user_id" form:"user_id"`
+	ProductID uint `gorm:"not null" json:"product_id" form:"product_id"`
+	Qty       int  `json:"qty" form:"qty"`
+	Subtotal  uint `json:"subtotal" form:"subtotal"`
 }
