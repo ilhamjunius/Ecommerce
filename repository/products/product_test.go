2eeac6b3eeca6fb0ca2b6febadd8299cfe92e561
Ilham Junius <ilhamjunius712@gmail.com>
2022-01-20 09:22:50 +0700

    add testing repo

--- repository/products/product_test.go
+++ repository/products/product_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestUsersRepo(t *testing.T) {
+func TestProductRepo(t *testing.T) {
 	config := configs.GetConfig()
 	db := utils.InitDB(config)
 
@@ -17,13 +17,25 @@ func TestUsersRepo(t *testing.T) {
 	db.AutoMigrate(&entities.Product{})
 
 	productRepo := NewProductRepo(db)
-
 	t.Run("Insert Product into Database", func(t *testing.T) {
 		var mockProduct entities.Product
 		mockProduct.Name = "bola"
 		mockProduct.Price = 10000
 		mockProduct.Stock = 10
-		mockProduct.CategoryID = 1
+		mockProduct.CategoryID = 2
+		mockProduct.Description = "bola basket"
+		db.Migrator().DropTable(&entities.Product{})
+		_, err := productRepo.Create(mockProduct)
+		assert.Error(t, err)
+
+	})
+	db.AutoMigrate(&entities.Product{})
+	t.Run("Insert Product into Database", func(t *testing.T) {
+		var mockProduct entities.Product
+		mockProduct.Name = "bola"
+		mockProduct.Price = 10000
+		mockProduct.Stock = 10
+		mockProduct.CategoryID = 2
 		mockProduct.Description = "bola basket"
 
 		res, err := productRepo.Create(mockProduct)
@@ -38,7 +50,49 @@ func TestUsersRepo(t *testing.T) {
 		assert.Nil(t, err)
 		assert.Equal(t, res, res)
 	})
+	db.Migrator().DropTable(&entities.Product{})
+	t.Run("Error Select Products from Database", func(t *testing.T) {
+		_, err := productRepo.GetAll()
+		assert.Error(t, err)
 
+	})
+	t.Run("Error Select Product from Database", func(t *testing.T) {
+		_, err := productRepo.Get(1)
+		assert.Error(t, err)
+
+	})
+	t.Run("Error Update User ", func(t *testing.T) {
+		var mockProduct entities.Product
+		mockProduct.Name = "bola ajaib"
+		mockProduct.Price = 50000
+		mockProduct.Stock = 1
+		mockProduct.CategoryID = 1
+		mockProduct.Description = "bola sihir"
+
+		_, err := productRepo.Update(mockProduct, 1)
+		assert.Error(t, err)
+
+	})
+	t.Run("Error Delete User", func(t *testing.T) {
+		_, err := productRepo.Delete(1)
+		assert.Error(t, err)
+
+	})
+	db.AutoMigrate(&entities.Product{})
+	t.Run("Insert Product into Database", func(t *testing.T) {
+		var mockProduct entities.Product
+		mockProduct.Name = "bola"
+		mockProduct.Price = 10000
+		mockProduct.Stock = 10
+		mockProduct.CategoryID = 2
+		mockProduct.Description = "bola basket"
+
+		res, err := productRepo.Create(mockProduct)
+		assert.Nil(t, err)
+		assert.Equal(t, mockProduct.Name, res.Name)
+		assert.Equal(t, 1, int(res.ID))
+
+	})
 	t.Run("Select Product from Database", func(t *testing.T) {
 		res, err := productRepo.Get(1)
 		assert.Nil(t, err)
