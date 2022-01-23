31be0c38803f5b4d2c75a6e7850b173cb52f0eb2
andrewptjio <andrewptjioo@gmail.com>
2022-01-23 16:58:25 +0800

    config for ec2

--- utils/mysqldriver.go
+++ utils/mysqldriver.go
@@ -11,13 +11,15 @@ import (
 )
 
 func InitDB(config *configs.AppConfig) *gorm.DB {
-	connectionString := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=utf8&parseTime=True&loc=Local",
-		config.Database.Username,
-		config.Database.Password,
-		config.Database.Address,
-		config.Database.Port,
-		config.Database.Name,
-	)
+	connectionString :=
+		fmt.Sprintf(
+			"%v:%v@tcp(%v:%v)/%v?charset=utf8&parseTime=True&loc=Local",
+			config.Username,
+			config.Password,
+			config.Address,
+			config.DB_Port,
+			config.Name,
+		)
 
 	db, err := gorm.Open(mysql.Open(connectionString), &gorm.Config{})
 
